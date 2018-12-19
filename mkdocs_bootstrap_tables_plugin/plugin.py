651760fca12da4cae521e8febd3e571488179eda
byrnereese <byrne@majordojo.com>
2018-12-19 13:12:59 -0800

    Initial add

---
+++ mkdocs_bootstrap_tables_plugin/plugin.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import re
+from timeit import default_timer as timer
+from datetime import datetime, timedelta
+
+from mkdocs import utils as mkdocs_utils
+from mkdocs.config import config_options, Config
+from mkdocs.plugins import BasePlugin
+
+class BootstrapTablesPlugin(BasePlugin):
+
+    config_scheme = (
+        ('param', config_options.Type(mkdocs_utils.string_types, default='')),
+    )
+
+    def __init__(self):
+        self.enabled = True
+        self.total_time = 0
+
+    def on_post_page(self, output_content, page, config):
+        output_content = re.sub(r"<table>", "<table class=\"table\">", output_content)
+        output_content = re.sub(r"<th>", "<th scope=\"col\">", output_content)
+        return output_content
+
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mkdocs-bootstrap-tables-plugin',
+    version='0.1.0',
+    description='A MkDocs plugin to add bootstrap classes to plan markdown generated tables.',
+    long_description='',
+    keywords='mkdocs bootstrap css',
+    url='https://github.com/byrnereese/mkdocs-bootstrap-tables-plugin',
+    author='Byrne Reese',
+    author_email='byrne@majordojo.com',
+    license='MIT',
+    python_requires='>=2.7',
+    install_requires=[
+        'mkdocs>=1.0.4'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'mkdocs.plugins': [
+            'bootstrap-tables = mkdocs_bootstrap_tables_plugin.plugin:BootstrapTablesPlugin'
+        ]
+    }
+)
